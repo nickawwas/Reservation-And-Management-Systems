fbf17d76ab23b39c33fa35cc476d6b2f0aa74eef
Nicholas Kawwas <68450354+nickawwas@users.noreply.github.com>
2020-08-25 13:46:17 -0400

    Create Outpatient.hpp

---
+++ Asgmnt4/Outpatient.hpp
@@ -0,0 +1,30 @@
+/*
+ * Outpatient.hpp
+ * Author: Nicholas Kawwas
+ * Completed: March, 2020
+ * Purpose: Outpatient Class
+*/
+
+#pragma once
+
+#include <iostream>
+#include "Doctor.h"
+
+using namespace std;
+
+class Outpatient : public Person {
+    private:
+        Doctor* Dr = new Doctor();
+        DateTime* appointmentDate = new DateTime();
+
+        static double fee;  //Hospital charge
+
+    public:
+        Outpatient(); //Default Constructor
+        Outpatient(string n, DateTime a, Doctor b); //Regular Constructor
+        Outpatient(const Outpatient&); //Copy Constructor
+        ~Outpatient(); //Destructor
+    
+        void print(); //Prints Outpatient
+};
+
