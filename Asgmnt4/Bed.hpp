4e2050dad3ddca0515e6aff646ba70969a38192a
Nicholas Kawwas <68450354+nickawwas@users.noreply.github.com>
2020-08-25 12:57:58 -0400

    Create Bed.hpp

---
+++ Asgmnt4/Bed.hpp
@@ -0,0 +1,27 @@
+/*
+ * Bed.hpp
+ * Author: Nicholas Kawwas
+ * Completed: March, 2020
+ * Purpose: Bed Class
+*/
+
+#pragma once
+
+class Date {
+    protected:
+        int day;
+        int month;
+        int year;
+    
+    public:
+        Date(); //Default Constructor
+        Date(int a, int b, int c); //Regular Constructor
+        Date(const Date&); //Copy Constructor
+        ~Date(); //Destructor
+    
+        int getDay() const; //Returns Day
+        int getMonth() const; //Return Month
+        int getyear() const; //Return Year
+
+};
+
