3d431add006a0788768b13b5cd5e3d44f27ddc7d
Nicholas Kawwas <68450354+nickawwas@users.noreply.github.com>
2020-08-25 12:11:29 -0400

    Update Passenger.cpp

--- Asgmnt2/Passenger.cpp
+++ Asgmnt2/Passenger.cpp
@@ -21,9 +21,9 @@ Passenger::Passenger(string n,int m,int d,int y) {
 }
 
 Passenger::Passenger(){
-    name="";
-    conum=0;
-    dob=nullptr;
+    name = "";
+    conum = 0;
+    dob = nullptr;
 }
 
 Passenger::~Passenger() {
@@ -36,42 +36,46 @@ Passenger::Passenger(const Passenger&p){
     dob = p.dob;
     conum = p.conum;
 
-    for(int i=0;i<conum;i++)
-        reserequest[i]=p.reserequest[i];
+    for(int i = 0;i < conum; i++)
+        reserequest[i] = p.reserequest[i];
 }
 
 void Passenger::printPassenger() const {
-    cout<<"Passenger: "<< name <<endl;
-    cout<<"Date of Birth: ";
+    cout << "Passenger: " << name << endl;
+    cout << "Date of Birth: ";
     dob->printDate();
 }
 
-void Passenger::setName(string n){ name=n;}
+void Passenger::setName(string n){ 
+    name=n;
+}
 
-string Passenger::getName() const{ return name;}
+string Passenger::getName() const { 
+    return name;
+}
 
 void Passenger::setRequest(int n) {
-    if(conum<=4){
-        reserequest[conum]=n;
+    if(conum <= 4){
+        reserequest[conum] = n;
         conum++;
     } else
-        cout<<"Invalid Operation More Than 4 Requests Already" << endl;
+        cout << "Invalid Operation More Than 4 Requests Already" << endl;
 }
 
 void Passenger::printReservations() const{
     cout << "Passenger " << name << " Has " << conum << " Reservations: " << endl;
 
-     for(int i=0;i<conum;i++)
+     for(int i = 0; i < conum; i++)
          cout << "Reservation number is " << reserequest[i] << endl;
 
      cout << endl;
 }
 
 void Passenger::removeReservation(int c) {
-    for(int i=0;i<conum;i++)
+    for(int i = 0;i < conum;i++)
         if(reserequest[i] == c) {
             for(int j = i;j < conum;j++)
-                reserequest[j] = reserequest[j+1];
+                reserequest[j] = reserequest[j + 1];
                 
         conum--;
         return;
